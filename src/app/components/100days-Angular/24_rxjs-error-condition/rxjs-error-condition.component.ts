923f2de495568c24eb04c638c338f578aa4677cf
Duong Minh Thuan <duongminhthuan1478@gmail.com>
2021-09-12 16:41:52 +0700

    Rename readme to note folders to clearly

---
+++ src/app/components/100days-Angular/24_rxjs-error-condition/rxjs-error-condition.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-rxjs-error-condition',
+  templateUrl: './rxjs-error-condition.component.html'
+})
+export class RxjsErrorConditionComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
