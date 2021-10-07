fdffa63e3e258740c1eb53ad94bfb45acef2906a
Duong Minh Thuan <duongminhthuan1478@gmail.com>
2021-10-07 16:29:40 +0700

    Apply  loading.. status for navigate in router at app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,3 +1,4 @@
+import { NavigationCancel, NavigationEnd, NavigationError, NavigationStart, Router } from '@angular/router';
 import { AfterViewInit, Component, OnInit, ChangeDetectorRef } from '@angular/core';
 import { UtilsService } from './shared/services/utils.service';
 
@@ -11,8 +12,13 @@ export class AppComponent implements OnInit{
 
   constructor( 
     public _utils: UtilsService,
-    private cdr: ChangeDetectorRef
-  ){  }
+    private cdr: ChangeDetectorRef,
+    private router: Router
+  ){
+    this.router.events.subscribe(event => {
+      this.handleLoadingBar(event)
+    });
+  }
 
   ngOnInit(): void {
    
@@ -20,7 +26,20 @@ export class AppComponent implements OnInit{
 
   ngAfterViewChecked(){
     this.cdr.detectChanges();
- }
+  }
 
-
+  private handleLoadingBar(event) {
+    if(event instanceof NavigationStart) {
+      this._utils.isLoading$.next(true);
+    }
+    else if(event instanceof NavigationEnd) {
+      this._utils.isLoading$.next(false);
+    } 
+    else if(event instanceof NavigationCancel) {
+      this._utils.isLoading$.next(false);
+    } 
+    else if(event instanceof NavigationError) {
+      this._utils.isLoading$.next(false);
+    }
+  }
 }
